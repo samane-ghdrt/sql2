6b3172a5717108a84ae9c37b5a50bb54f59a109d
samane <samane.ghodrati@gmail.com>
2025-05-10 03:00:32 -0700

    add

---
+++ Q1/conectDb/conect.js
@@ -0,0 +1,13 @@
+import {mysql} from 'mysql';
+
+export var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+ 
